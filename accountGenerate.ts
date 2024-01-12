4ec8191c14abf8c28886c4b4f575ab13ed77731f
Ohno0704 <68172397+aishoku17127@users.noreply.github.com>
2024-01-12 17:41:16 +0900

    first commit

---
+++ accountGenerate.ts
@@ -0,0 +1,13 @@
+import { RepositoryFactoryHttp, Account } from "symbol-sdk";
+import { firstValueFrom } from 'rxjs';
+const example = async (): Promise<void> => { //   
+const nodeUrl = "http://sym-test-01.opening-line.jp:3000"; //   2 
+const repositoryFactory = new RepositoryFactoryHttp(nodeUrl);
+const networkType = await firstValueFrom(repositoryFactory.getNetworkType());
+const alice = Account.generateNewAccount(networkType); //   3 
+  console.dir(alice,{depth: null}); //   4
+  console.log(`address: ${alice.address.plain()}`);
+  console.log(`publicKey ${alice.publicKey}`);
+  console.log(`privateKey ${alice.privateKey}`);
+};
+example().then(); //   5
